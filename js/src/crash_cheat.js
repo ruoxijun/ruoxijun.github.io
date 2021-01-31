3f89a3cf30c461347c7e4faafd7df02e54ce9e0e
ruoxijun <1460662245@qq.com>
2021-01-31 14:26:58 +0800

    Site updated: 2021-01-31 14:26:28

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -3,12 +3,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/TEP.ico");
-        document.title = '╭(°A°`)╮ 别走呀亲！ ~';
+        $('[rel="icon"]').attr('href', "/images/ku.png");
+        document.title = '╭(°A°`)╮ 别走呀亲~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
+        $('[rel="icon"]').attr('href', "/images/favicon-32x32-next.png");
         document.title = '(ฅ>ω<*ฅ) 欢迎回来亲~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
